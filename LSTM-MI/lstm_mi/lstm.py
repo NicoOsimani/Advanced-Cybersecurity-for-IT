ca0e53b710dc9ae9b420d35564daee13544ac547
Nico Osimani <NicoOsimani>
2021-05-18 23:26:20 +0200

    caricamento utils.py per il preprocess e la divisione in fold

--- LSTM-MI/lstm_mi/lstm.py
+++ LSTM-MI/lstm_mi/lstm.py
@@ -18,28 +18,45 @@ from sklearn.metrics import precision_score, recall_score, classification_report
 from datetime import datetime
 
 # todo set params
-csv_dataset_path = "drive/MyDrive/Cyber Security/dga_domains_full.csv" #Advanced-Cybersecurity-for-IT/LSTM-MI/lstm_mi/traindga5.csv  drive/MyDrive/Cyber Security/dga_domains_full.csv
+folder_dataset_path = "drive/MyDrive" #drive/MyDrive
+dataset_name = "dga_domains"
+start_fold = 1
+end_fold = 10 #10
 out_path = "drive/MyDrive" #drive/MyDrive
 test_name = "dga_domains"
 max_epoch = 20 #20
-nfolds = 10 #10
 batch_size = 128 #128
 patience = 5
 min_delta = 0.0
 
-if csv_dataset_path == "drive/MyDrive/Cyber Security/dga_domains_full.csv":
-    alexa_class = 14
-else:
-    alexa_class = 20
 
-def get_data(): 
+def get_data(fold):
 	"""Read data from file (Traning, testing and validation) to process"""
-	data= []
-	with open(csv_dataset_path, "r") as f:
-		reader = csv.reader(f)
-		for row in reader:
-			data.append(row) 
-	return data
+	X_train = []
+	X_test = []
+	y_train = []
+	y_test = []
+	in_train_file = open(folder_dataset_path + "/" + dataset_name + "_train_fold_" + str(fold) + ".csv")
+	in_test_file = open(folder_dataset_path + "/" + dataset_name + "_test_fold_" + str(fold) + ".csv")
+	csvreader = csv.reader(in_train_file, delimiter=";")
+	for row in csvreader:
+		y_train.append(int(row[0]))
+		line = []
+		for i in range(1, len(row)):
+			line.append(int(row[i]))
+		X_train.append(line)
+	csvreader = csv.reader(in_test_file, delimiter=";")
+	for row in csvreader:
+		y_test.append(int(row[0]))
+		line = []
+		for i in range(1, len(row)):
+			line.append(int(row[i]))
+		X_test.append(line)
+	X_train = np.array(X_train)
+	X_test = np.array(X_test)
+	y_train = np.array(y_train)
+	y_test = np.array(y_test)
+	return X_train, X_test, y_train, y_test
 
 def build_binary_model(max_features, maxlen):
     """Build LSTM model for two-class classification"""
@@ -54,20 +71,6 @@ def build_binary_model(max_features, maxlen):
 
     return model
 
-def build_multiclass_model(max_features, maxlen):
-    """Build multiclass LSTM model for multiclass classification"""
-    model = Sequential()
-    model.add(Embedding(max_features, 128, input_length=maxlen))
-    model.add(LSTM(128))
-    model.add(Dropout(0.5))
-    model.add(Dense(38))
-    model.add(Activation('softmax'))
-
-    model.compile(loss='sparse_categorical_crossentropy',optimizer='rmsprop')
-
-    return model
-
-
 def create_class_weight(labels_dict,mu):
     """Create weight based on the number of domain name in the dataset"""
     total = np.sum(labels_dict.values())
@@ -80,14 +83,13 @@ def create_class_weight(labels_dict,mu):
 
     return class_weight
 
-def classifaction_report_csv(report,precision,recall,f1_score,accuracy,fold,classification):
+def classifaction_report_csv(report,precision,recall,f1_score,accuracy,fold):
     """Generate the report to data processing"""
     with open(out_path + '/LSTM-MI_' + test_name + '_results.csv', 'a') as f:
         report_data = []
         lines = report.split('\n')
         row = {}
         row['class'] =  "fold %u" % (fold)
-        row['f1_score'] = classification
         report_data.append(row)
         for line in lines[2:44]:
             row = {}
@@ -122,54 +124,15 @@ def classifaction_report_csv(report,precision,recall,f1_score,accuracy,fold,clas
         dataframe = pd.DataFrame.from_dict(report_data)
         dataframe.to_csv(f, index = False)
 
-def run(max_epoch=max_epoch, nfolds=nfolds, batch_size=batch_size, patience=patience, min_delta=min_delta):
+def run(max_epoch=max_epoch, start_fold=start_fold, end_fold=end_fold, batch_size=batch_size, patience=patience, min_delta=min_delta):
     """Run train/test on logistic regression model"""
-
-    #Begin preprocessing stage
-    #Read data to process
-    indata = get_data()
-
-    # Extract data and labels
-    binary_labels = [x[0] for x in indata]
-    X = [x[2] for x in indata]
-    labels = [x[1] for x in indata]
-    # Generate a dictionary of valid characters
-    valid_chars = {x:idx+1 for idx, x in enumerate(set(''.join(X)))}
-    max_features = len(valid_chars) + 1
-    maxlen = np.max([len(x) for x in X])
-
-    # Convert characters to int and pad
-    X = [[valid_chars[y] for y in x] for x in X]
-    X = sequence.pad_sequences(X, maxlen=maxlen)
-   
-	# Convert labels to 0-1 for binary class
-    y_binary = np.array([0 if x == 'legit' else 1 for x in binary_labels])
-    # Convert labels to 0-(number of classes - 1) for multi class
-    valid_class = {i:indx for indx, i in enumerate(set(labels))}
-    y = [valid_class[x] for x in labels]
-    y = np.array(y)
-    #End preprocessing stage
-
-
-    #Begin two-class classification stage
-    #Divide the dataset into training + holdout (80%) and testing (20%) dataset
-    sss = StratifiedShuffleSplit(n_splits=nfolds, test_size=0.2, random_state=0)
-    fold =0
-    for train, test in sss.split(X,y_binary,y):
-        print "Fold %u/%u" % (fold+1, nfolds)
-        fold = fold+1
-        X_train, X_test, y_train, y_test, y_dga_train, y_dga_test = X[train], X[test], y_binary[train], y_binary[test], y[train], y[test]
-        y_dga =[]
-        X_dga =[]
-        for i in range(len(y_dga_train)):
-        	if y_dga_train[i]!= alexa_class:
-        		y_dga.append(y_dga_train[i])
-        		X_dga.append(X_train[i])
-        X_dga =np.array(X_dga)
-        y_dga =np.array(y_dga)       
+    for fold in range (start_fold, end_fold + 1):
+        X_train, X_test, y_train, y_test = get_data(fold)
+        
+        print "Fold %u/%u" % (fold, end_fold) 
 
         #Build the model for two-class classification stage
-        model = build_binary_model(max_features, maxlen)
+        model = build_binary_model(40, 73) #40, 73
         
         print "Training the model for two-class classification stage..."
         sss1 = StratifiedShuffleSplit(n_splits=1, test_size=0.05, random_state=0)
@@ -185,7 +148,7 @@ def run(max_epoch=max_epoch, nfolds=nfolds, batch_size=batch_size, patience=pati
         loss_val = []
         loss_train = []
         stop_train = 0
-        #20
+        
         for ep in range(max_epoch):
             if stop_train < patience:
                 print "Epoch %u/%u" % (ep+1, max_epoch)
@@ -251,94 +214,20 @@ def run(max_epoch=max_epoch, nfolds=nfolds, batch_size=batch_size, patience=pati
         #Calculate the final result
         class_names = ["legit", "dga"]
 
-        classification = "binary classification"
-
         score = f1_score(y_test, y_result, average="macro")
         precision = precision_score(y_test, y_result, average="macro")
         recall = recall_score(y_test, y_result, average="macro")
         report = classification_report(y_test, y_result, target_names=class_names, digits=4)
         acc = accuracy_score(y_test, y_result)
-        classifaction_report_csv(report,precision,recall,score,acc,fold, classification)
+        classifaction_report_csv(report,precision,recall,score,acc,fold)
         print "\nBinary classification results"
         print '\nClassification report:\n', report
         print 'F1 score:', score
         print 'Recall:', recall
         print 'Precision:', precision
         print 'Acc:', acc
-'''
-        #Begin multiclass classification stage
-        #Build the model for multiclass classification stage
-        model_dga = build_multiclass_model(max_features, maxlen)
-        print "Training the model in multiclass classification..."
         
-        sss2 = StratifiedShuffleSplit(n_splits=1, test_size=0.05, random_state=0)
-        for train, test in sss2.split(X_dga, y_dga):
-            X_train, X_holdout, y_train, y_holdout = X_dga[train], X_dga[test], y_dga[train], y_dga[test]
-        
-        # Caculate class weight 
-        labels_dict=collections.Counter(y_train)
-        class_weight = create_class_weight(labels_dict,0.3)
-        print 'Class weight for multiclass classification:'
-        print class_weight
-        best_acc = 0.0
-        #20
-        for ep in range(max_epoch):
-            model_dga.fit(X_train, y_train, batch_size=batch_size, nb_epoch=1, class_weight=class_weight)
-            y_pred= model_dga.predict_proba(X_holdout)
-            y_result = [np.argmax(x) for x in y_pred]  
-            t_acc= accuracy_score(y_holdout, y_result)
-            if t_acc > best_acc:
-                best_model_dga = model_dga
-                best_acc = t_acc
-        #Save the model for multiclass classification stage
-        model_json = best_model_dga.to_json()
-        name_file = "model_multi.json"
-        name_file2 = "model_multi.h5"
-        with open(name_file, "w") as json_file:
-            json_file.write(model_json)
-        # serialize weights to HDF5
-            best_model_dga.save_weights(name_file2)
-        print("Saved multiclass model to disk")
-
-        y_pred = best_model.predict_proba(X_test)
-        y_result = [alexa_class if(x<=0.5) else 0 for x in y_pred]
-        
-        X_dga_test =[]
-        y_dga_test_labels =[]
-        for i in range(len(y_result)):
-            if y_result[i] == 0:
-                X_dga_test.append(X_test[i])
-                y_dga_test_labels.append(y_dga_test[i])
-        X_dga_test = np.array(X_dga_test)
-        y_dga_test_labels = np.array(y_dga_test_labels)
-        y_pred_dga = best_model_dga.predict_proba(X_dga_test)
-        y_result_dga = [np.argmax(x) for x in y_pred_dga]
-        #End of multiclass classification stage
-
-        j = 0
-        for i in range(len(y_result)):
-            if y_result[i] != alexa_class:
-                y_result[i] = y_result_dga[j]
-                j = j+1
-
-        #Calculate the final result
-        class_names = set(labels)
-
-        classification = "multi-classification"
-
-        score = f1_score(y_dga_test, y_result,average="macro")
-        precision = precision_score(y_dga_test, y_result,average="macro")
-        recall = recall_score(y_dga_test, y_result,average="macro")
-        report = classification_report(y_dga_test, y_result, target_names=class_names, digits=4)
-        acc= accuracy_score(y_dga_test, y_result)
-        classifaction_report_csv(report,precision,recall,score,acc,fold, classification)
-        print "\nMulti-classification results"
-        print '\nClassification report:\n', report
-        print 'F1 score:', score
-        print 'Recall:', recall
-        print 'Precision:', precision
-        print 'Acc:', acc      
-'''           
    
 if __name__ == "__main__":
     run()
+
