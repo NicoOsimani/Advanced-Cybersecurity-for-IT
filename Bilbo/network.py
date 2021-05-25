323fe2a89b1ec86c3798ed91888eaf0f883d6feb
marzdeveloper <daniel.23@live.it>
2021-05-25 17:51:59 +0200

    Update bilbo

--- Bilbo/network.py
+++ Bilbo/network.py
@@ -142,7 +142,7 @@ def train_eval_test(model, start_fold, end_fold):
             df[str(fold)] = pd.Series(metrics, index=metrics.keys(), name=str(fold))
             df.to_csv(out_path + "metrics.csv")
         except:
-            pd.DataFrame(metrics, index=metrics.keys(), columns= [str(fold)]).to_csv(out_path + "metrics.csv")
+            pd.DataFrame(metrics, index=metrics.keys()).to_csv(out_path + "metrics.csv")
         predicted = np.round(best_model.predict(x=x_test, batch_size=batch_size), decimals=0)
         metrics1 = classification_report(y_test, predicted, output_dict=True, target_names=['alexa', 'dga'])
         try:
@@ -150,7 +150,7 @@ def train_eval_test(model, start_fold, end_fold):
             df1[str(fold)] = pd.Series(metrics1, index=metrics1.keys(), name=str(fold))
             df1.to_csv(out_path + "metrics1.csv")
         except:
-            pd.DataFrame(metrics1, index=metrics1.keys(), columns= [str(fold)]).to_csv(out_path + "metrics1.csv")
+            pd.DataFrame(metrics1, index=metrics1.keys()).to_csv(out_path + "metrics1.csv")
         np.savetxt(out_path + 'confusion_matrix' + str(fold) + '.csv', confusion_matrix(y_test, predicted), delimiter=',',  fmt='%i')
         print(accuracy_score(y_test, predicted))
 
